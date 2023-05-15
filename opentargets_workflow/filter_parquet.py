81d04e31da1276ff21d5cb1c42c198b8bdb16d4b
jamespcook36 <130742968+jamespcook36@users.noreply.github.com>
2023-05-15 12:29:02 +0100

    Create filter_parquet.py

---
+++ opentargets_workflow/filter_parquet.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import pyarrow.parquet as pq
+from sys import argv
+
+snplist = argv[3]
+with open(snplist, 'r') as f:
+  lines = f.readlines()
+
+lines = [l.split(':', 1)[1] for l in lines]
+
+lines = [l.split('_', 1)[0] for l in lines]
+
+#pathval = ${params.parquetfolder}
+#fileval = $parquetfile
+
+#pathfile = pathval+fileval
+
+# Read the parquet file
+#df = pd.read_parquet('${params.parquetfolder}$parquetfile')
+
+parquet_file = argv[1]
+pqfile = pq.read_table(parquet_file)
+
+# pqfile = pq.read_table('${params.parquetfolder}parquetfile')
+
+df = pqfile.to_pandas()
+
+# Iterate through the lines in the text file and search for them in the parquet file
+
+results = []
+
+for line in lines:
+  result = df[df['position'] == int(line.strip())]
+  results.append(result)
+
+output_file = argv[2] + '.tsv'
+
+# Concatenate the resulting DataFrames and save to a new file
+final_result = pd.concat(results)
+final_result.to_csv(output_file, index=False, sep="\t")
